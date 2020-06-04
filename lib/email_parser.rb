d65048b4a1647d9d8871630b8afd64975a92f5cd
Anthony George <ageorge530@gmail.com>
2020-06-04 21:37:39 +0000

    Automatically backed up by Learn

---
+++ lib/email_parser.rb
@@ -0,0 +1,21 @@
+class EmailAddressParser
+
+  attr_reader :emails
+
+
+
+  def initialize(emails)
+
+    @emails = emails
+
+  end
+
+
+
+  def parse
+
+    emails.split(/, | /).uniq
+
+  end
+
+end
